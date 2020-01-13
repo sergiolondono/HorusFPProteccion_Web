b69f69df37d4efe9f3c27e8499c2f35a18b7410d
Sergio Andres Londono Gallo <Sergio.Londono@carvajal.com>
2020-01-13 13:42:01 -0500

    20200113. seguridad para dobel captura.

--- src/app/dynamic-form-builder/atoms/textbox.ts
+++ src/app/dynamic-form-builder/atoms/textbox.ts
@@ -11,7 +11,10 @@ import { FieldsFunctionalityService } from 'src/app/services/fields-functionalit
         class="form-control form-control-sm"  [id]="field.name" [name]="field.name" 
         [formControlName]="field.name" maxlength="100"
         (keyup)="this.fieldService.validateFieldRecapture(field, form)"
-        oninput="this.value = this.value.toUpperCase()">
+        oninput="this.value = this.value.toUpperCase()"
+        (blur)="setInputType($event, form)"
+        onfocus="this.type='text'"
+        >
         <textarea *ngIf="field.multiline" [class.is-invalid]="isDirty && !isValid" 
         [formControlName]="field.name" [id]="field.name"
         rows="9" class="form-control" [placeholder]="field.placeholder"></textarea>
@@ -25,6 +28,32 @@ export class TextBoxComponent {
     get isDirty() { return this.form.controls[this.field.name].dirty; }
     
     constructor(public fieldService: FieldsFunctionalityService) {
+    }
+
+    setInputType(event:any, form){
+      const entries = Object.keys(form.controls)
+      console.log(entries);
+      event.target.type = 'text';
+      var contador = 0;
+      for(var i = 0; i < entries.length; i++)
+      {
+        // console.log(entries[i]);
+        // console.log(event.target.name);
+        var result= entries[i].indexOf(event.target.name);
+        console.log(result);
+        if(result == 0){
+          contador++;
+        }
+      }
+
+      if(contador > 1){
+        if(event.target.name.indexOf('recaptura') > 0){
+          event.target.type = 'text';
+        }
+        else{
+          event.target.type = 'password';
+        }  
+      }         
     }
 
 }
